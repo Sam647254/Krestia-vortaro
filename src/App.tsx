af38a6c5a3f616d59d1b7f13e1e4ad4967b5ce4d
Sam Wang <samofcorinth@gmx.ca>
2020-01-30 23:17:17 -0800

    Aldonis serĉilon

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import {BrowserRouter} from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   return (
+      <BrowserRouter>
+         <div className="titolo">
+            <h1>
+               voliste vol Krestia
+            </h1>
+            <p className="subtitolo">
+               Krestia dictionary
+            </p>
+         </div>
+
+         <form className="serĉilo">
+            <input type="text" className="serĉiTeksto"/>
+            <input type="submit" className="serĉiButono"
+            value="Search"/>
+         </form>
+      </BrowserRouter>
+   );
 }
 
 export default App;
